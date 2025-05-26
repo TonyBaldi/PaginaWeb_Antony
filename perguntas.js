39d8b46d39ccd38269b76c418b48abb8a353e023
TonyBaldi <baldissera.antony@escola.pr.gov.br>
2025-05-26 08:28:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'História',
+    'Qual a data da independência do Brasil?',
+    '7 de Setembro de 1822'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
